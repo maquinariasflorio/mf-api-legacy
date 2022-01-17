68bd795d74b8db008cc60fadae3e51cce86ffe28
Jesus Barrientos <jesus.barrientosg.95@gmail.com>
2022-01-17 11:19:14 -0300

    Added amounPerDay and amountPerTravel for Trucks inside Booking module

---
+++ migrations/20220116151937-UpdateInitialViewForAdminRole.js
@@ -0,0 +1,21 @@
+module.exports = {
+    async up(db, client) {
+
+        await db.collection('role').updateOne( { name: 'administrator' }, {
+            $set: {
+                initialView: 'report-daily',
+            },
+        } )
+    
+    },
+
+    async down(db, client) {
+
+        await db.collection('role').updateOne( { name: 'administrator' }, {
+            $set: {
+                initialView: 'reports',
+            },
+        } )
+    
+    },
+}
